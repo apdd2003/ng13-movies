3093c4a8df2d996520af7010737b319140dfbd51
samsk <apdd2003@yahoo.co.in>
2022-05-12 22:00:41 +0530

    video embed component

--- src/app/components/video-embed/video-embed.component.ts
+++ src/app/components/video-embed/video-embed.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-video-embed',
@@ -10,9 +10,13 @@ export class VideoEmbedComponent implements OnInit {
 
   @Input() site: string = "Youtube";
   @Input() key:string | null = null;
+
+  videoUrl: SafeResourceUrl='';
+
   constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+    this.videoUrl=this.getSafeUrl('https://www.youtube.com/embed/'+this.key);
   }
   getSafeUrl(url: string){
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
